e11980aed077e2141dedb64175d601ff85c7ea5f
KANJO <contact@amine.tokyo>
2025-01-26 09:46:30 +0100

    chore: bump version of lanyard.ts

--- lib/api/routes/root.ts
+++ lib/api/routes/root.ts
@@ -5,7 +5,7 @@ export function rootRoutes(fastify: FastifyInstance) {
     return {
       message:
         "Hello! If you'd like to use my API, feel free to DM me on Discord at kanjo.app. This API is a TypeScript rewrite of Lanyard by KANJO, inspired by the original Lanyard created by Phineas.",
-      version: "1.0.0",
+      version: "1.0.5",
       documentation: "Coming soon",
     };
   });
