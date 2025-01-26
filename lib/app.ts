96eb0472a4fe3e4363315d47d7f906b0747c19b2
KANJO <contact@amine.tokyo>
2025-01-26 10:22:21 +0100

    chore: remove whitespacing

--- lib/app.ts
+++ lib/app.ts
@@ -7,7 +7,6 @@ async function main() {
   try {
     await client.login(config.discord.token);
     await server.listen({ port: config.api.port, host: "0.0.0.0" });
-
     Logger.info(`Server listening on port ${config.api.port}`);
   } catch (error) {
     console.error("Failed to start:", error);
