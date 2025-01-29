c7f61dd6638fc9c03ba9365ae5d569036e746590
KANJO <contact@amine.tokyo>
2025-01-29 19:55:45 +0100

    chore: wtf nahhh its not what i excepted

--- lib/discord/bot/events/ready.ts
+++
@@ -1,6 +0,0 @@
-import { Logger } from "#/utils/logger";
-import { Client } from "discord.js";
-
-export function handleClientReady(client: Client) {
-  Logger.success(`Logged in as ${client.user?.tag}!`);
-}
